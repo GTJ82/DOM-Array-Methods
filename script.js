e28a63c095896d63ec4fd74db8b1ad0c869f01ef
Josh Eskew <jeskew82@gmail.com>
2021-02-27 17:51:09 -0500

    Added a few more fucntions to the script. All buttions work except for Calculate wealth button

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const main = document.getElementById('main')
 const addUserBtn = document.getElementById('add-user')
 const doubleBtn = document.getElementById('double')
-const showMillionairs = document.getElementById('show-millionairs')
+const showMillionairsBtn = document.getElementById('show-millionaires')
 const sortBtn = document.getElementById('sort')
 const calculateWealthBtn = document.getElementById('calculate-wealth')
 
 let data = [];
 
-getRandomUser();
-getRandomUser();
-getRandomUser();
-
+// Generates random user data from API
 async function getRandomUser() {
     const res = await fetch('https://randomuser.me/api');
     const data = await res.json();
@@ -23,7 +20,7 @@ async function getRandomUser() {
     addData(newUser);
 }
 
-
+// Adds new data to data array
 function addData(obj) {
     data.push(obj)
 
@@ -37,7 +34,39 @@ function updateDOM(providedData = data) {
     providedData.forEach(item => {
         const element = document.createElement('div');
         element.classList.add('person');
-        element.innerHTML = `<strong>${item.name}</strong>${item.money}`;
+        element.innerHTML = `<strong>${item.name}</strong>${formatMoney(item.money)}`;
         main.appendChild(element);
     })
 }
+
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+function doubleMoney() {
+    data = data.map(user => {
+        return { ...user, money: user.money * 2 }
+    })
+
+    updateDOM();
+}
+
+function sortByRichest() {
+    data.sort((a, b) => b.money - a.money)
+
+    updateDOM();
+}
+
+function onlyMillionairs() {
+    data = data.filter(user => user.money > 1000000)
+
+    updateDOM();
+}
+
+
+// Event listeners
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairsBtn.addEventListener('click', onlyMillionairs);
+
